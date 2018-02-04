d2237628c72a94da9e4345fc325b2ed19c83baa5
egweber2 <ethangweber@gmail.com>
2018-02-03 17:44:42 -0800

    added comments

--- graph.py
+++ graph.py
@@ -9,17 +9,23 @@ Created on Sat Feb  3 15:36:30 2018
 import networkx as nx
 import pandas as pd
 import random
+
+"""Creates dummy data of 5000 or so names taken from list online"""
 names = list(set(list(pd.read_csv('names.csv')['firstname'])))
 
+"""Initializes graph"""
 G = nx.Graph()
 
 for i in range(10000):
+    """"Creates graph with 10,000 edges""""
     G.add_edge(names[random.randint(0,len(names)-1)], names[random.randint(0,len(names)-1)])
 
+"""implements pagerank"""
 ranks = nx.pagerank(G)
 
 df = pd.DataFrame()
 
+"""puts those ranks in a dataframe"""
 df['names'] =  list(ranks.keys())
 def get_rank(name, rank_dict):
     try:
@@ -29,4 +35,5 @@ def get_rank(name, rank_dict):
     
 df['score'] = list(map(lambda x: get_rank(x, ranks), list(df.names)))
 
+"""Sorts high to low"""
 df.sort_values(['score'], ascending = 0)
