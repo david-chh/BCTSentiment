5400bef968c1f4893ba610c681134b66adfe01d8
egweber2 <ethangweber@gmail.com>
2018-02-04 11:03:38 -0800

    update for ryan

---
+++ FrontEnd/angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'InfluRank, A CryptoMarket Sentiment Indicator';
+  subTitle = "InfluRank was developed with the Optimus Market Indicator Development Tool"
+}
---
+++ FrontEnd/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', score: 50, url: 'url.com' },
+  { id: 12, name: 'Narco', score: 50, url: 'url.com'  },
+  { id: 13, name: 'Bombasto', score: 50, url: 'url.com'  },
+  { id: 14, name: 'Celeritas', score: 50, url: 'url.com'  }
+];
